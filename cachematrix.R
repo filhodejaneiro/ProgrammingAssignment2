eaad8294fdb69b6ced1bc038763e866a98e28ad5
Iakov Vokhmintsev <zauberer@gmail.com>
2015-06-22 00:15:51 +0500

    Added code to the stub functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work together, helping to cache
+## the result of potentially time-consuming inverse of a matrix
 
-## Write a short comment describing this function
+## This funcion returns a helper object, which is a matrix plus cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL # cache for the inverse
+    set <- function(y) { ## setter for the argument matrix
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x ## getter for the argument matrix
+    setinverse <- function(inverse) inv <<- inverse ## setter for the cache
+    getinverse <- function() inv ## getter for the cache
+    list (set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of a matrix stored in a helper object
+## as returned by the makeCacheMatrix() function, retrieving it from the cache if
+## the cached value exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
 }
